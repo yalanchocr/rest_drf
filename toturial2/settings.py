0aa41ff5e533fa3864a226d3c9e6e8d499da2352
o.kazemian <o.kazemian@ofogh.com>
2024-06-22 19:17:22 +0330

    quick srart drf

--- toturial2/settings.py
+++ toturial2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'snippets',
+    'quickstart',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- toturial2/urls.py
+++ toturial2/urls.py
@@ -16,11 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 import snippets.views
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', snippets.views.create_post),
     path('', include('snippets.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
